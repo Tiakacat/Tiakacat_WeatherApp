816e969c1f81edabe73d7d3839f2926c66d7c21f
Tiakacat <106970182+Tiakacat@users.noreply.github.com>
2022-07-20 16:35:30 +0200

    Navigator removed to prevent default asking for the access to the user's location

--- src/project.js
+++ src/project.js
@@ -91,7 +91,7 @@ function retrievePosition(position) {
   let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
   axios.get(url).then(showWeather);
 }
-navigator.geolocation.getCurrentPosition(retrievePosition);
+// navigator.geolocation.getCurrentPosition(retrievePosition);
 
 function getCurrentLocation(event) {
   event.preventDefault();
